10d35a36cb1f1db64342b2f485aaebb63744a3b8
Leonardo Quinones <leito25@gmail.com>
2026-05-21 16:20:01 -0500

    Creation of the Widget Stack Tags, a way to tagging on Unreal.

---
+++ Source/ownueui/Public/FrontendGameplayTags.h
@@ -0,0 +1,13 @@
+// Leonardo Quinones All Rights Reserved
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace FrontendGameplayTags
+{
+	OWNUEUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Modal);
+	OWNUEUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameMenu);
+	OWNUEUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameHud);
+	OWNUEUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Frontend);
+}
