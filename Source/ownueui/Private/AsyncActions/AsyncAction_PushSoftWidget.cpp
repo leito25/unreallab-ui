414b4c789850e0517531dd5aac43f0796a4ef234
Leonardo Quinones <leito25@gmail.com>
2026-05-25 18:49:05 -0500

    Createing the Async Action for the blueprint

--- Source/ownueui/Private/AsyncActions/AsyncAction_PushSoftWidget.cpp
+++ Source/ownueui/Private/AsyncActions/AsyncAction_PushSoftWidget.cpp
@@ -3,3 +3,24 @@
 
 #include "AsyncActions/AsyncAction_PushSoftWidget.h"
 
+UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(const UObject* WorldContextObject,
+	APlayerController* OwningPlayerController, TSoftClassPtr<UWidget_ActivatableBase> InSoftWidgetClass,
+	UPARAM(meta = (Categories = "Frontend.WidgetStack")) FGameplayTag InWidgetStackTag, bool bFocusOnNewlyPushedWidget)
+{
+	checkf(!InSoftWidgetClass.IsNull(), TEXT("PushSoftWidgetToStack was passed a null soft widget class"));
+	
+	if (GEngine)// Check in runtime
+	{
+		// 
+		if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+		{
+			UAsyncAction_PushSoftWidget* Node = NewObject<UAsyncAction_PushSoftWidget>();
+			
+			Node->RegisterWithGameInstance(World);
+			
+			return Node;
+		}
+	}
+	
+	return nullptr;
+}
--- Source/ownueui/Public/AsyncActions/AsyncAction_PushSoftWidget.h
+++ Source/ownueui/Public/AsyncActions/AsyncAction_PushSoftWidget.h
@@ -3,15 +3,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "EditorUtilityLibrary.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "GameplayTagContainer.h"
 #include "AsyncAction_PushSoftWidget.generated.h"
+
+class UWidget_ActivatableBase;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPushSoftWidgetDelegate, UWidget_ActivatableBase*, PushedWidget);
 
 /**
  * 
  */
 UCLASS()
-class OWNUEUI_API UAsyncAction_PushSoftWidget : public UEditorUtilityBlueprintAsyncActionBase
+class OWNUEUI_API UAsyncAction_PushSoftWidget : public UBlueprintAsyncActionBase
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContextObject",
+		HidePin = "WorldContextObject", 
+		BlueprintInternalUseOnly = "true", 
+		DisplayName = "Push Soft Widget To Widget Stack"))
+	static UAsyncAction_PushSoftWidget* PushSoftWidget(const UObject* WorldContextObject, 
+		APlayerController* OwningPlayerController,
+		TSoftClassPtr<UWidget_ActivatableBase> InSoftWidgetClass,
+		UPARAM(meta = (Categories = "Frontend.WidgetStack")) FGameplayTag InWidgetStackTag,
+		bool bFocusOnNewlyPushedWidget = true
+		);
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnPushSoftWidgetDelegate OnWidgetCreatedBeforePush;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnPushSoftWidgetDelegate AfterPush;
 	
 };
