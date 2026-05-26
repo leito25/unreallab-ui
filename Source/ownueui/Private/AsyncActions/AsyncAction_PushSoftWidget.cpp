11ca50974d2ffa83fad379973d4f9d5ed3c43be2
Leonardo Quinones <leito25@gmail.com>
2026-05-25 20:06:39 -0500

    Impleneting the Async Action and running a test widget

--- Source/ownueui/Private/AsyncActions/AsyncAction_PushSoftWidget.cpp
+++ Source/ownueui/Private/AsyncActions/AsyncAction_PushSoftWidget.cpp
@@ -2,6 +2,8 @@
 
 
 #include "AsyncActions/AsyncAction_PushSoftWidget.h"
+#include "Subsystems/FrontendUISubsystem.h"
+#include "Widgets/Widget_ActivatableBase.h"
 
 UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(const UObject* WorldContextObject,
 	APlayerController* OwningPlayerController, TSoftClassPtr<UWidget_ActivatableBase> InSoftWidgetClass,
@@ -15,6 +17,11 @@ UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(const U
 		if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
 		{
 			UAsyncAction_PushSoftWidget* Node = NewObject<UAsyncAction_PushSoftWidget>();
+			Node->CachedOwningWorld = World;
+			Node->CachedOwningPC = OwningPlayerController;
+			Node->CachedSoftWidgetClass = InSoftWidgetClass;
+			Node->CachedWidgetStackTag = InWidgetStackTag;
+			Node->bCachedFocusOnNewlyPushedWidget = bFocusOnNewlyPushedWidget;
 			
 			Node->RegisterWithGameInstance(World);
 			
@@ -23,4 +30,38 @@ UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(const U
 	}
 	
 	return nullptr;
+}
+
+void UAsyncAction_PushSoftWidget::Activate()
+{
+	UFrontendUISubsystem* FrontendUISubsystem = UFrontendUISubsystem::Get(CachedOwningWorld.Get());
+	
+	FrontendUISubsystem->PushSoftWidgetToStackAsync(CachedWidgetStackTag, CachedSoftWidgetClass,
+		[this](EAsyncPushWidgetState InPushState, UWidget_ActivatableBase* PushedWidget)
+		{
+			switch(InPushState)
+			{
+			case EAsyncPushWidgetState::OnCreateBeforePush:
+				PushedWidget->SetOwningPlayer(CachedOwningPC.Get());
+				OnWidgetCreatedBeforePush.Broadcast(PushedWidget);
+				break;
+			case EAsyncPushWidgetState::AfterPush:
+				AfterPush.Broadcast(PushedWidget);
+				
+				if(bCachedFocusOnNewlyPushedWidget)
+				{
+					if (UWidget* WidgetToFocus = PushedWidget->GetDesiredFocusTarget())
+					{
+						WidgetToFocus->SetFocus();
+					}
+				}
+				
+				SetReadyToDestroy();
+				
+				break;
+			default:
+				break;
+			}
+		}
+	);
 }
--- Source/ownueui/Public/AsyncActions/AsyncAction_PushSoftWidget.h
+++ Source/ownueui/Public/AsyncActions/AsyncAction_PushSoftWidget.h
@@ -31,10 +31,20 @@ public:
 		bool bFocusOnNewlyPushedWidget = true
 		);
 	
+	//~ Begin UBlueprintAsyncActionBase Interface
+	virtual void Activate() override;
+	//~ End UBlueprintAsyncActionBase Interface
+	
 	UPROPERTY(BlueprintAssignable)
 	FOnPushSoftWidgetDelegate OnWidgetCreatedBeforePush;
 	
 	UPROPERTY(BlueprintAssignable)
 	FOnPushSoftWidgetDelegate AfterPush;
 	
+private:
+	TWeakObjectPtr<UWorld> CachedOwningWorld;
+	TWeakObjectPtr<APlayerController> CachedOwningPC;
+	TSoftClassPtr<UWidget_ActivatableBase> CachedSoftWidgetClass;
+	FGameplayTag CachedWidgetStackTag;
+	bool bCachedFocusOnNewlyPushedWidget = false;
 };
